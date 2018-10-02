128e1242e9b4cb5f9bb04ece15b9c1122b907920
jesseokeya <jesseokeya@gmail.com>
2018-10-02 18:13:42 -0400

    changes

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM node:alpine as builder
-WORKDIR '/app'
+WORKDIR /app
 COPY package*.json ./
 RUN npm install
 COPY ./ ./
 RUN npm run build
+
+COPY ./ ./
 
 FROM nginx
 EXPOSE 80
---
+++ build/precache-manifest.4174b49a04b56c636e92fd6bd70767ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jesseokeya/docker-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c7987bf85bf8f6552a82",
+    "url": "/jesseokeya/docker-react/static/js/runtime~main.c7987bf8.js"
+  },
+  {
+    "revision": "ef7897fdd0b00b4509cd",
+    "url": "/jesseokeya/docker-react/static/js/main.ef7897fd.chunk.js"
+  },
+  {
+    "revision": "23828c7aef184be86558",
+    "url": "/jesseokeya/docker-react/static/js/1.23828c7a.chunk.js"
+  },
+  {
+    "revision": "ef7897fdd0b00b4509cd",
+    "url": "/jesseokeya/docker-react/static/css/main.b2ab208c.chunk.css"
+  },
+  {
+    "revision": "68ed2e0d6a806221e8e300589702b8ca",
+    "url": "/jesseokeya/docker-react/index.html"
+  }
+];
