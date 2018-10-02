399228910f41e1c52bc90c65bb59679cf54928e2
jesseokeya <jesseokeya@gmail.com>
2018-10-02 13:43:23 -0400

    changes

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,10 @@
-# ----- Builder ----
-
 FROM node:alpine as builder
-
-WORKDIR /app
-
+WORKDIR '/app'
 COPY package*.json ./
-
 RUN npm install
-
 COPY . .
-
 RUN npm run build
 
-# ----- Nginx ------
-
 FROM nginx
-
-# For port mapping in aws elastic beanstalk
 EXPOSE 80
-
-COPY --from=builder /app/build/ /usr/share/nginx/html
+COPY --from=builder /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -8,18 +8,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi there lol!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
+        <p className="App-intro">
+          I was changed on the Feature branch
+        </p>
       </div>
     );
   }
